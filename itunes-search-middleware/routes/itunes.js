838dfdb6c7933bcc84b6cac1bfec56c6cb73eaf1
Stefany Biskup <teffybiskup@gmail.com>
2021-05-26 21:07:03 -0300

    Removing default files and adjusting GET method in order to pass params

--- itunes-search-middleware/routes/itunes.js
+++ itunes-search-middleware/routes/itunes.js
@@ -1,13 +1,20 @@
-var express = require('express');
-var fetch = require('node-fetch');
+var express = require("express");
+var fetch = require("node-fetch");
 var router = express.Router();
 
-/* GET Jack Johnson data through iTunes API. */
-router.get('/', async function (req, res, next) {
-  var apiUrl = 'https://itunes.apple.com/search?term=jack+johnson';
+/* GET Albums data through iTunes API. */
+router.get("/:query", async function (req, res, next) {
+  const query = req.params.query;
+  var apiUrl = `https://itunes.apple.com/search?term=${query}&entity=album`;
   var fetchResponse = await fetch(apiUrl);
   var json = await fetchResponse.json();
   res.json(json);
+});
+
+/* USE Show error message. */
+router.use(function (err, req, res, next) {
+  console.log(err.stack);
+  res.status(500).send("Something went wrong. Please, try again.");
 });
 
 module.exports = router;
