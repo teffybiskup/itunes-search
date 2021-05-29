9f56d25d01339f39815d2e0c2749ab38b053fe3b
Stefany Biskup <teffybiskup@gmail.com>
2021-05-28 21:58:02 -0300

    Removing duplicated albums from json.results

--- itunes-search-middleware/routes/itunes.js
+++ itunes-search-middleware/routes/itunes.js
@@ -5,9 +5,19 @@ var router = express.Router();
 /* GET Albums data through iTunes API. */
 router.get("/:query", async function (req, res, next) {
   const query = req.params.query;
-  var apiUrl = `https://itunes.apple.com/search?term=${query}&entity=album`;
+  var apiUrl = `https://itunes.apple.com/search?term=${query}&entity=album&attribute=allArtistTerm`;
   var fetchResponse = await fetch(apiUrl);
   var json = await fetchResponse.json();
+
+  json.results = json.results
+    .reduce((acc, currentObject) => {
+      if(!acc.find(object => object['collectionName'] === currentObject['collectionName'])) {
+        acc.push(currentValue);
+      }
+      return acc;
+    }, [])
+  json.resultCount = json.results.length;
+
   res.json(json);
 });
 
