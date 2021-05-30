5f2eb98c1e8cbcaf7b5d1c7f29ab6726ee1cec49
Stefany Biskup <teffybiskup@gmail.com>
2021-05-30 16:04:54 -0300

    Adjusting ESLint warnings

--- vue-itunes-search/tests/unit/services/iTunes.spec.ts
+++ vue-itunes-search/tests/unit/services/iTunes.spec.ts
@@ -1,9 +1,10 @@
-import axios from "axios";
-import MockAdapter from "axios-mock-adapter";
 import sinon from "sinon";
+import MockAdapter from "axios-mock-adapter";
+import axios, { AxiosResponse } from "axios";
 import { expect } from "chai";
 import { iTunesService } from "@/services/iTunes";
 import { albumsMock, albumsResponseMock } from "../../mocks/albums";
+import { Album } from "@/types";
 
 describe("iTunes Service", () => {
   const axiosMock = new MockAdapter(axios);
@@ -14,7 +15,9 @@ describe("iTunes Service", () => {
       axiosMock.onGet().reply(200, albumsResponseMock);
       const iTunesServiceStub = sinon
         .stub(iTunesService, "getAlbums")
-        .callsFake((): any => albumsMock);
+        .callsFake(
+          () => albumsMock as unknown as Promise<AxiosResponse<Album[]>>
+        );
 
       const response = await iTunesService.getAlbums("tame impala");
 
