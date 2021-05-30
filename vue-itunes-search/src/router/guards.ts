365a39fd9d48b27f826609cad314778630a5653c
Stefany Biskup <teffybiskup@gmail.com>
2021-05-30 02:19:58 -0300

    Improving loadMediaData() NavigationGuard RegEx

--- vue-itunes-search/src/router/guards.ts
+++ vue-itunes-search/src/router/guards.ts
@@ -3,7 +3,7 @@ import { ASSIGN_ALBUMS } from "@/store/action-types";
 import { NavigationGuard } from "vue-router";
 
 export const loadMediaData: NavigationGuard = async (to, from, next) => {
-  if (!/[^a-zA-Z0-9 ]/gi.test(to.params.term as string))
+  if (/[A-z0-9.\'!\- ]/gi.test(to.params.term as string))
     await store.dispatch(ASSIGN_ALBUMS, to.params.term);
   next();
 };
