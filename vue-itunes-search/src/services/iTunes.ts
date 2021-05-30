b9eefe6ca032250f931f072ca3171806c498e681
Stefany Biskup <teffybiskup@gmail.com>
2021-05-30 15:40:32 -0300

    Adjusting iTunes Service file

---
+++ vue-itunes-search/src/services/iTunes.ts
@@ -0,0 +1,14 @@
+import axios, { AxiosResponse } from "axios";
+import { Album } from "@/types";
+
+export class iTunesService {
+  public static async getAlbums(
+    term: string
+  ): Promise<AxiosResponse<Album[] | null>> {
+    const response = await axios({
+      url: `itunes/${term}`,
+      baseURL: "http://localhost:3000/",
+    });
+    return response.data.results;
+  }
+}
